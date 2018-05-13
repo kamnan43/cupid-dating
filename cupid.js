93174b9f227fc266a31ab1dc258ee9370dd92dbc
Sitthi <kamnan43@gmail.com>
2018-05-13 13:38:25 +0700

    first commit

---
+++ cupid.js
@@ -0,0 +1,358 @@
+const config = require('./config.json');
+const baseURL = config.BASE_URL;
+const lineSdk = require('@line/bot-sdk');
+const line = new lineSdk.Client(config);
+const firebase = require("firebase-admin");
+var firebaseConfig = config.firebase;
+firebaseConfig.credential = firebase.credential.cert(require(firebaseConfig.serviceAccountFile));
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var membersRef = database.ref("/members");
+
+module.exports = {
+  sendGreetingMessage: (replyToken) => {
+    return replyText(
+      replyToken,
+      [
+        `ยินดีต้อนรับสู่ Cupid Dating : บริการหาคู่ทางไลน์`,
+        `เงื่อนไขการใช้บริการ\n` +
+        `1. ระบบอาจบันทึกข้อมูลส่วนตัวของคุณ ได้แก่ ชื่อโปรไฟล์ รูปโปรไฟล์ สถานะโปรไฟล์ เพื่อใช้ในการให้บริการ\n` +
+        `2. ข้อมูลส่วนตัวของคุณจะแสดงต่อผู้ใช้อื่นในระบบ เฉพาะคนที่ระบุความต้องการตรงตามที่คุณระบุเท่านั้น\n` +
+        `3. ระบบอยู่ในช่วงระหว่างการทดสอบให้บริการ`,
+      ]
+    ).then(() => {
+      replyConfirm(replyToken, 'เงื่อนไขการใช้งาน', 'คุณยอมรับเงื่อนไขการใช้งานหรือไม่', 'TOS_YES', 'TOS_NO');
+    });
+  },
+
+  saveNewMember: (userId, replyToken) => {
+    var memberRef = database.ref("/members/" + userId);
+    memberRef.set({
+      userId: userId,
+      status: 0
+    });
+  }
+
+  // handleText(message, replyToken, source) {
+  //   const buttonsImageURL = `${baseURL}/static/buttons/1040.jpg`;
+
+  //   switch (message.text) {
+  //     case 'profile':
+  //       if (source.userId) {
+  //         const downloadPath = path.join(__dirname, 'downloaded', `${source.userId}.jpg`);
+  //         const previewPath = path.join(__dirname, 'downloaded', `${source.userId}-preview.jpg`);
+  //         return client.getProfile(source.userId)
+  //           .then((profile) => {
+  //             replyText(
+  //               replyToken,
+  //               [
+  //                 `Display name: ${profile.displayName}`,
+  //                 `Status message: ${profile.statusMessage || '-'}`,
+  //               ]
+  //             ).then(() => {
+  //               downloadProfilePicture(profile.pictureUrl, downloadPath);
+  //             }).then(() => {
+  //               cp.execSync(`convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`);
+  //               client.pushMessage(
+  //                 source.userId,
+  //                 {
+  //                   type: 'image',
+  //                   originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+  //                   previewImageUrl: baseURL + '/downloaded/' + path.basename(previewPath),
+  //                 }
+  //               ).catch((error) => { console.log('error', error) });
+  //             });
+  //           });
+  //       } else {
+  //         return replyText(replyToken, 'Bot can\'t use profile API without user ID');
+  //       }
+  //     case 'buttons':
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'template',
+  //           altText: 'Buttons alt text',
+  //           template: {
+  //             type: 'buttons',
+  //             thumbnailImageUrl: buttonsImageURL,
+  //             title: 'My button sample',
+  //             text: 'Hello, my button',
+  //             actions: [
+  //               { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+  //               { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+  //               { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+  //               { label: 'Say message', type: 'message', text: 'Rice=米' },
+  //             ],
+  //           },
+  //         }
+  //       );
+  //     case 'confirm':
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'template',
+  //           altText: 'Confirm alt text',
+  //           template: {
+  //             type: 'confirm',
+  //             text: 'Do it?',
+  //             actions: [
+  //               { label: 'Yes', type: 'message', text: 'Yes!' },
+  //               { label: 'No', type: 'message', text: 'No!' },
+  //             ],
+  //           },
+  //         }
+  //       )
+  //     case 'carousel':
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'template',
+  //           altText: 'Carousel alt text',
+  //           template: {
+  //             type: 'carousel',
+  //             columns: [
+  //               {
+  //                 thumbnailImageUrl: buttonsImageURL,
+  //                 title: 'hoge',
+  //                 text: 'fuga',
+  //                 actions: [
+  //                   { label: 'Go to line.me', type: 'uri', uri: 'https://line.me' },
+  //                   { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+  //                 ],
+  //               },
+  //               {
+  //                 thumbnailImageUrl: buttonsImageURL,
+  //                 title: 'hoge',
+  //                 text: 'fuga',
+  //                 actions: [
+  //                   { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
+  //                   { label: 'Say message', type: 'message', text: 'Rice=米' },
+  //                 ],
+  //               },
+  //             ],
+  //           },
+  //         }
+  //       );
+  //     case 'image carousel':
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'template',
+  //           altText: 'Image carousel alt text',
+  //           template: {
+  //             type: 'image_carousel',
+  //             columns: [
+  //               {
+  //                 imageUrl: buttonsImageURL,
+  //                 action: { label: 'Go to LINE', type: 'uri', uri: 'https://line.me' },
+  //               },
+  //               {
+  //                 imageUrl: buttonsImageURL,
+  //                 action: { label: 'Say hello1', type: 'postback', data: 'hello こんにちは' },
+  //               },
+  //               {
+  //                 imageUrl: buttonsImageURL,
+  //                 action: { label: 'Say message', type: 'message', text: 'Rice=米' },
+  //               },
+  //               {
+  //                 imageUrl: buttonsImageURL,
+  //                 action: {
+  //                   label: 'datetime',
+  //                   type: 'datetimepicker',
+  //                   data: 'DATETIME',
+  //                   mode: 'datetime',
+  //                 },
+  //               },
+  //             ]
+  //           },
+  //         }
+  //       );
+  //     case 'datetime':
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'template',
+  //           altText: 'Datetime pickers alt text',
+  //           template: {
+  //             type: 'buttons',
+  //             text: 'Select date / time !',
+  //             actions: [
+  //               { type: 'datetimepicker', label: 'date', data: 'DATE', mode: 'date' },
+  //               { type: 'datetimepicker', label: 'time', data: 'TIME', mode: 'time' },
+  //               { type: 'datetimepicker', label: 'datetime', data: 'DATETIME', mode: 'datetime' },
+  //             ],
+  //           },
+  //         }
+  //       );
+  //     case 'imagemap':
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'imagemap',
+  //           baseUrl: `${baseURL}/static/rich`,
+  //           altText: 'Imagemap alt text',
+  //           baseSize: { width: 1040, height: 1040 },
+  //           actions: [
+  //             { area: { x: 0, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/manga/en' },
+  //             { area: { x: 520, y: 0, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/music/en' },
+  //             { area: { x: 0, y: 520, width: 520, height: 520 }, type: 'uri', linkUri: 'https://store.line.me/family/play/en' },
+  //             { area: { x: 520, y: 520, width: 520, height: 520 }, type: 'message', text: 'URANAI!' },
+  //           ],
+  //         }
+  //       );
+
+  //     case 'bye':
+  //       switch (source.type) {
+  //         case 'user':
+  //           return replyText(replyToken, 'Bot can\'t leave from 1:1 chat');
+  //         case 'group':
+  //           return replyText(replyToken, 'Leaving group')
+  //             .then(() => client.leaveGroup(source.groupId));
+  //         case 'room':
+  //           return replyText(replyToken, 'Leaving room')
+  //             .then(() => client.leaveRoom(source.roomId));
+  //       }
+
+
+  //     default:
+  //       console.log(`Echo message to ${replyToken}: ${message.text}`);
+  //       return replyText(replyToken, message.text);
+  //   }
+  // }
+
+  // handleImage(message, replyToken) {
+  //   const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.jpg`);
+  //   const previewPath = path.join(__dirname, 'downloaded', `${message.id}-preview.jpg`);
+
+  //   return downloadContent(message.id, downloadPath)
+  //     .then((downloadPath) => {
+  //       // ImageMagick is needed here to run 'convert'
+  //       // Please consider about security and performance by yourself
+  //       cp.execSync(`convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`);
+
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'image',
+  //           originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+  //           previewImageUrl: baseURL + '/downloaded/' + path.basename(previewPath),
+  //         }
+  //       );
+  //     });
+  // }
+
+  // handleVideo(message, replyToken) {
+  //   const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.mp4`);
+  //   const previewPath = path.join(__dirname, 'downloaded', `${message.id}-preview.jpg`);
+
+  //   return downloadContent(message.id, downloadPath)
+  //     .then((downloadPath) => {
+  //       // FFmpeg and ImageMagick is needed here to run 'convert'
+  //       // Please consider about security and performance by yourself
+  //       cp.execSync(`convert mp4:${downloadPath}[0] jpeg:${previewPath}`);
+
+  //       return line.replyMessage(
+  //         replyToken,
+  //         {
+  //           type: 'video',
+  //           originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+  //           previewImageUrl: baseURL + '/downloaded/' + path.basename(previewPath),
+  //         }
+  //       );
+  //     });
+  // }
+
+  // handleAudio(message, replyToken) {
+  //   const downloadPath = path.join(__dirname, 'downloaded', `${message.id}.m4a`);
+
+  //   return downloadContent(message.id, downloadPath)
+  //     .then((downloadPath) => {
+  //       var getDuration = require('get-audio-duration');
+  //       var audioDuration;
+  //       getDuration(downloadPath)
+  //         .then((duration) => { audioDuration = duration; })
+  //         .catch((error) => { audioDuration = 1; })
+  //         .finally(() => {
+  //           return line.replyMessage(
+  //             replyToken,
+  //             {
+  //               type: 'audio',
+  //               originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+  //               duration: audioDuration * 1000,
+  //             }
+  //           );
+  //         });
+  //     });
+  // }
+
+  // downloadContent(messageId, downloadPath) {
+  //   return client.getMessageContent(messageId)
+  //     .then((stream) => new Promise((resolve, reject) => {
+  //       const writable = fs.createWriteStream(downloadPath);
+  //       stream.pipe(writable);
+  //       stream.on('end', () => resolve(downloadPath));
+  //       stream.on('error', reject);
+  //     }));
+  // }
+
+  // downloadProfilePicture(pictureUrl, downloadPath) {
+  //   return new Promise((resolve, reject) => {
+  //     http.get(pictureUrl, function (response) {
+  //       const writable = fs.createWriteStream(downloadPath);
+  //       response.pipe(writable);
+  //       response.on('end', () => resolve(downloadPath));
+  //       response.on('error', reject);
+  //     });
+  //   });
+  // }
+
+  // handleLocation(message, replyToken) {
+  //   return line.replyMessage(
+  //     replyToken,
+  //     {
+  //       type: 'location',
+  //       title: message.title,
+  //       address: message.address,
+  //       latitude: message.latitude,
+  //       longitude: message.longitude,
+  //     }
+  //   );
+  // }
+
+  // handleSticker(message, replyToken) {
+  //   return line.replyMessage(
+  //     replyToken,
+  //     {
+  //       type: 'sticker',
+  //       packageId: message.packageId,
+  //       stickerId: message.stickerId,
+  //     }
+  //   );
+  // }
+};
+// simple reply function
+function replyText(token, texts) {
+  texts = Array.isArray(texts) ? texts : [texts];
+  return line.replyMessage(
+    token,
+    texts.map((text) => ({ type: 'text', text }))
+  );
+}
+
+function replyConfirm(token, title, text, yesKey, noKey) {
+  return line.replyMessage(
+    token,
+    {
+      type: 'template',
+      altText: title,
+      template: {
+        type: 'confirm',
+        text: text,
+        actions: [
+          { label: 'Yes', type: 'message', text: yesKey },
+          { label: 'No', type: 'message', text: noKey },
+        ],
+      },
+    }
+  )
+}
---
+++ index.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const lineSdk = require('@line/bot-sdk');
+const express = require('express');
+const fs = require('fs');
+const git = require('./git-deploy');
+const config = require('./config.json');
+const cupid = require('./cupid.js');
+const http = require('http');
+const https = require('https');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use('/static', express.static('static'));
+app.use('/downloaded', express.static('downloaded'));
+app.post('/git', function (req, res) {
+  res.status(200).end();
+  git.deploy({
+    origin: "origin",
+    branch: "master"
+  });
+});
+// app.post('/webhooks', lineSdk.middleware(config), (req, res) => {
+app.post('/webhooks', (req, res) => {
+  if (!Array.isArray(req.body.events)) {
+    return res.status(500).end();
+  }
+  Promise.all(req.body.events.map(handleEvent))
+    .then(() => res.end())
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+  switch (event.type) {
+    case 'message':
+      const message = event.message;
+      switch (message.type) {
+        case 'text':
+          return handleText(message, event.replyToken, event.source);
+        // case 'image':
+        //   return handleImage(message, event.replyToken);
+        // case 'video':
+        //   return handleVideo(message, event.replyToken);
+        // case 'audio':
+        //   return handleAudio(message, event.replyToken);
+        // case 'location':
+        //   return handleLocation(message, event.replyToken);
+        // case 'sticker':
+        //   return handleSticker(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+
+    case 'follow':
+      return cupid.sendGreetingMessage(event.replyToken);
+
+    case 'unfollow':
+      return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
+
+    case 'postback':
+      let data = event.postback.data;
+      if (data === 'DATE' || data === 'TIME' || data === 'DATETIME') {
+        data += `(${JSON.stringify(event.postback.params)})`;
+      }
+      return replyText(event.replyToken, `Got postback: ${data}`);
+
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
+  }
+}
+
+function handleText(message, replyToken, source) {
+  switch (message.text) {
+    case 'TOS_YES':
+      if (source.userId) {
+        cupid.saveNewMember(source.userId, replyToken);
+      } else {
+        return replyText(replyToken, 'Error : NO_USER_ID');
+      }
+  
+    default:
+      console.log(`Echo message to ${replyToken}: ${message.text}`);
+      return replyText(replyToken, message.text);
+  }
+}
+
+
+// listen on port
+const port = config.PORT;
+
+// var certOptions = {
+//   key: fs.readFileSync('./cert/privkey.pem'),
+//   cert: fs.readFileSync('./cert/fullchain.pem')
+// };
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+// https.createServer(certOptions, app).listen(port + 800);
