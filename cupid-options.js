e6fc62eceac72595b9ca1794726179531781f410
Sitthi <kamnan43@gmail.com>
2018-05-13 15:42:29 +0700

    fix

--- cupid-options.js
+++ cupid-options.js
@@ -1,26 +1,27 @@
-export const tosActions = [
-  createPostBackOption('ยอมรับ', 'TOS_YES'),
-  createPostBackOption('ไม่ยอมรับ', 'TOS_NO')
-];
-export const genderActions = [
-  createPostBackOption('ชาย', 'GENDER_M'),
-  createPostBackOption('หญิง', 'GENDER_F'),
-  createPostBackOption('อื่นๆ', 'GENDER_X')
-];
-export const ageActions = [
-  createPostBackOption('18-22 ปี', 'AGE_18'),
-  createPostBackOption('23-27 ปี', 'AGE_23'),
-  createPostBackOption('28-32 ปี', 'AGE_28'),
-  createPostBackOption('33 ขึ้นไป', 'AGE_33')
-];
-export const partnerGenderActions = [
-  createPostBackOption('ชาย', 'PARTNER_GENDER_M'),
-  createPostBackOption('หญิง', 'PARTNER_GENDER_F'),
-  createPostBackOption('อื่นๆ', 'PARTNER_GENDER_X')
-];
-export const partnerAgeActions = [
-  createPostBackOption('18-22 ปี', 'PARTNER_AGE_18'),
-  createPostBackOption('23-27 ปี', 'PARTNER_AGE_23'),
-  createPostBackOption('28-32 ปี', 'PARTNER_AGE_28'),
-  createPostBackOption('33 ขึ้นไป', 'PARTNER_AGE_33')
-];
+module.exports = {
+  tosActions: [
+    createPostBackOption('ยอมรับ', 'TOS_YES'),
+    createPostBackOption('ไม่ยอมรับ', 'TOS_NO')
+  ],
+  genderActions: [
+    createPostBackOption('ชาย', 'GENDER_M'),
+    createPostBackOption('หญิง', 'GENDER_F'),
+    createPostBackOption('อื่นๆ', 'GENDER_X')
+  ],
+  ageActions: [
+    createPostBackOption('18-22 ปี', 'AGE_18'),
+    createPostBackOption('23-27 ปี', 'AGE_23'),
+    createPostBackOption('28-32 ปี', 'AGE_28'),
+    createPostBackOption('33 ขึ้นไป', 'AGE_33')
+  ],
+  partnerGenderActions: [
+    createPostBackOption('ชาย', 'PARTNER_GENDER_M'),
+    createPostBackOption('หญิง', 'PARTNER_GENDER_F'),
+    createPostBackOption('อื่นๆ', 'PARTNER_GENDER_X')
+  ], partnerAgeActions: [
+    createPostBackOption('18-22 ปี', 'PARTNER_AGE_18'),
+    createPostBackOption('23-27 ปี', 'PARTNER_AGE_23'),
+    createPostBackOption('28-32 ปี', 'PARTNER_AGE_28'),
+    createPostBackOption('33 ขึ้นไป', 'PARTNER_AGE_33')
+  ]
+}
