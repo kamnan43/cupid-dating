5e84863966aa522daf3b1f287459df18331e0bf0
Sitthi <kamnan43@gmail.com>
2018-05-19 14:53:12 +0700

    createImageCarouselMessage

--- cupid.js
+++ cupid.js
@@ -623,11 +623,11 @@ function createProfileMessage(altText, profile) {
 }
 
 function createImageCarouselMessage(altText, lists) {
-  console.log('createImageCarouselMessage');
   var columns = lists.map(element => {
     var title = (element.displayName || 'ไม่มีชื่อ');
     return lineHelper.createImageCarouselColumns(title, getProfileUrl(element.userId), element.userId);
   });
+  console.log('createImageCarouselMessage', columns);
   return lineHelper.createImageCarouselMessage(altText, columns)
 }
 
