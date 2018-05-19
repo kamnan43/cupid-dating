4ca451d2e29505b1ddc4fb912efe807d139cef99
Sitthi <kamnan43@gmail.com>
2018-05-19 11:37:00 +0700

    candidateInfo

--- cupid.js
+++ cupid.js
@@ -365,7 +365,9 @@ function viewCandidateProfile(userId, replyToken, candidateUserId) {
       return getUserInfo(candidateUserId)
     })
     .then((candidateInfo) => {
+      console.log('candidateInfo A',candidateInfo)
       candidateInfo.isFreind = (candidateRelation === 'LOVE' && memberRelation === candidateRelation);
+      console.log('candidateInfo B',candidateInfo)
       try {
         line.replyMessage(replyToken, [createProfileMessage(`เราคิดว่า คุณอาจอยากรู้จักเพื่อนใหม่เหล่านี้`, candidateInfo)]);
       } catch (e) {
